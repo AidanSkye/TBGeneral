6c6b526f9300ea6e344808388b22d4336c5070e3
Thebestharrison1221 <harydunn17@gmail.com>
2025-03-26 17:41:11 +0000

    Vanish command updated - mobs dont see vanish player - chests dont open for non vanish players - tab prefix if vanished - permissions - config
    TPA commands updated
    - permissions
    - config
    
    MSG commands updated
    - permissions

---
+++ src/main/java/org/TBCreates/TBGeneral/commands/admin/MessageSpyCommand.java
@@ -0,0 +1,40 @@
+package org.TBCreates.TBGeneral.commands.admin;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class MessageSpyCommand implements CommandExecutor {
+
+    public static final Set<Player> spyingAdmins = new HashSet<>();
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!(sender instanceof Player)) {
+            sender.sendMessage(ChatColor.RED + "Only players can use this command.");
+            return true;
+        }
+
+        Player player = (Player) sender;
+
+        if (!player.hasPermission("tbgeneral.messagespy")) {
+            player.sendMessage(ChatColor.RED + "You do not have permission to use this command.");
+            return true;
+        }
+
+        if (spyingAdmins.contains(player)) {
+            spyingAdmins.remove(player);
+            player.sendMessage(ChatColor.YELLOW + "MessageSpy has been " + ChatColor.RED + "disabled.");
+        } else {
+            spyingAdmins.add(player);
+            player.sendMessage(ChatColor.YELLOW + "MessageSpy has been " + ChatColor.GREEN + "enabled.");
+        }
+
+        return true;
+    }
+}
